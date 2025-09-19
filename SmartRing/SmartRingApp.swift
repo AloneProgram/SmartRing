9cec5ee0ab8ace56a48142b4d601baa43aa66421
zhouzhijie <18127088272@163.com>
2025-09-19 20:24:12 +0800

    Initial Commit

---
+++ SmartRing/SmartRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartRingApp.swift
+//  SmartRing
+//
+//  Created by liulilan on 2025/9/19.
+//
+
+import SwiftUI
+
+@main
+struct SmartRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
